ef43f0e734ac30c3e3cd6bc5e97c9001470cfc41
Carissa Wagner <wagner.carissa@gmail.com>
2018-04-18 18:14:09 -0700

    adds express to package.json and converts index.js to express syntax

--- index.js
+++ index.js
@@ -2,51 +2,56 @@ const http = require("http");
 const fs = require("fs");
 const gear = require('./lib/gear_list.js');
 const querystring = require('querystring');
+const express = require("express");
+const app = express();
 
-http.createServer((req,res) => {
-  const path = req.url.toLowerCase();
-  const pathstart = path.indexOf('?') == -1 ? 
-        path : 
-        path.slice(0, path.indexOf('?'));
-  switch(pathstart) {
-    case '/':
-      fs.readFile('public/home.html', (err, data) => {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(data);
-      });
-      break;
-    case '/about':
-      fs.readFile('public/about.html', (err, data) => {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(data);
-      });
-      break;
-    case '/get':
-      const getid = querystring.parse(path.slice(5)).id;
-      const getresult = gear.get(getid);
-      if (getresult) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Searching for id #' + getid + ':' + '\n' + JSON.stringify(getresult));
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // set location for static files
+app.use(require("body-parser").urlencoded({extended: true})); // parse form submissions
+
+app.get('/', (req, res) => {
+  res.type('text/html');
+  res.sendFile(__dirname + '/public/home.html');
+});
+
+app.get('/about', (req, res) => {
+  res.type('text/html');
+  res.sendFile(__dirname + '/public/about.html');
+});
+
+app.get('/get*', (req, res) => {
+  const getid = querystring.parse(req.url.toLowerCase().slice(5)).id;
+  const getresult = gear.get(getid);
+  if (getresult) {
+    res.type('text/plain');
+    res.send('Searching for id #' + getid + ':' + '\n' + JSON.stringify(getresult));
+  }
+  else {
+    res.type('text/plain');
+    res.send('Item not found');
+  };
+
+})
+
+app.get('/delete*', (req, res) => {
+  const delid = querystring.parse(req.url.toLowerCase().slice(8)).id;
+  const delresult = gear.delete(delid);
+  if (delresult) {
+    res.type('text/plain');
+    res.send('Id #' + delid + ' removed');
+  }
+  else {
+    res.type('text/plain');
+    res.send('Id #' + delid + ' not found');
       }
-      else {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Item not found');
-      };
-      break;
-    case '/delete':
-      const delid = querystring.parse(path.slice(8)).id;
-      const delresult = gear.delete(delid);
-      if (delresult) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Id #' + delid + ' removed');
-      }
-      else {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Id #' + delid + ' not found');
-      }
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found');
-      break;
-    }
-}).listen(process.env.PORT || 3000);
+})
+
+app.use( (req, res) => {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Express started');
+});
