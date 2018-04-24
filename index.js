ef6c12b7bfb5001dff063a37a83919da20e9f3d2
Carissa Wagner <wagner.carissa@gmail.com>
2018-04-23 22:04:47 -0700

    Adds search form on home page, links to item details, delete links, and more! For Assignment 3

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const querystring = require('querystring');
 const express = require("express");
 const app = express();
 
+let handlebars = require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html'}));
+app.set("view engine", ".html");
+
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public')); // set location for static files
 app.use(require("body-parser").urlencoded({extended: true})); // parse form submissions
 
 app.get('/', (req, res) => {
   res.type('text/html');
-  res.sendFile(__dirname + '/public/home.html');
+  res.render('home', {gear: gear.getAll()});
 });
 
 app.get('/about', (req, res) => {
@@ -19,8 +23,8 @@ app.get('/about', (req, res) => {
   res.sendFile(__dirname + '/public/about.html');
 });
 
-app.get('/get*', (req, res) => {
-  const getid = querystring.parse(req.url.toLowerCase().slice(5)).id;
+app.get('/get', (req, res) => {
+  const getid = req.query.id;
   const getresult = gear.get(getid);
   if (getresult) {
     res.type('text/plain');
@@ -30,20 +34,17 @@ app.get('/get*', (req, res) => {
     res.type('text/plain');
     res.send('Item not found');
   };
+});
 
-})
-
-app.get('/delete*', (req, res) => {
-  const delid = querystring.parse(req.url.toLowerCase().slice(8)).id;
+app.get('/delete', (req, res) => {
+  const delid = req.query.id;
   const delresult = gear.delete(delid);
-  if (delresult) {
-    res.type('text/plain');
-    res.send('Id #' + delid + ' removed');
-  }
-  else {
-    res.type('text/plain');
-    res.send('Id #' + delid + ' not found');
-      }
+  res.render('delete', {result: delresult, items: gear.getAll().length, id: delid});
+});
+
+app.get('/details', (req, res) => {
+  let result = gear.get(req.query.id);
+  res.render('details', {result: result});
 })
 
 app.use( (req, res) => {
