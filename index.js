67a922afc2f1c284a81f64084e5632b217ab7402
Carissa Wagner <wagner.carissa@gmail.com>
2018-05-25 16:17:09 -0700

    Adds API routes and an add method

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.set("view engine", ".html");
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public')); // set location for static files
 app.use(require("body-parser").urlencoded({extended: true})); // parse form submissions
+app.use('/api', require('cors')());
 
 app.get('/', (req, res) => {
   gearMethods.getAll().then((items) => {
@@ -40,6 +41,41 @@ app.get('/delete', (req, res) => {
 app.get('/details', (req, res) => {
   gearMethods.get(req.query.id).then((item) => {
     res.render('details', {result: item});
+  }).catch((err) => {
+    return next(err);
+  });
+});
+
+app.get('/api/gear', (req, res) => {
+  gearMethods.getAll().then((items) => {
+    res.json(items);
+  }).catch((err) => {
+    return next(err);
+  });
+});
+
+app.get('/api/item/:id', (req, res) => {
+  gearMethods.get(req.params.id).then((item) => {
+    res.json(item);
+  }).catch((err) => {
+    return next(err);
+  });
+});
+
+app.get('/api/item/delete/:id', (req, res) => {
+  gearMethods.delete(req.params.id).then((result) => {
+    Gear.count((err, count) => {
+      if (err) return next(err);
+      res.json({deletedCount: result.n, items: count, id: req.params.id});
+    });
+  }).catch((err) => {
+    return next(err);
+  });
+});
+
+app.post('/api/add', (req, res) => {
+  gearMethods.add(req.body).then((result) => {
+    res.json(result);
   }).catch((err) => {
     return next(err);
   });
